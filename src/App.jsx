f935b2e882bc1f717cd6ccb0dec3a34cb5b53077
Marwan Hisham <marwanhiisham@outlook.com>
2022-03-25 18:06:56 +0200

    initialize the app with vite and tailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <h1 className="text-5xl font-sans font-bold">Artificial Intelligence</h1>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: "'Ginora Sans', sans-serif",
+      body: "'Gilroy-Regular', sans-serif",
+    },
+    colors: {},
+    extend: {},
+  },
+  plugins: [],
+};
